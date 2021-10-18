b1307e7a7cf1f85909772c6df75171bcae1cd8c7
faisal-t <faizalmahromi35@gmail.com>
2021-10-18 22:37:57 +0700

    add random string for images

--- handler/user.go
+++ handler/user.go
@@ -141,7 +141,7 @@ func (h *userHandler) UploadAvatar(c *gin.Context) {
 	UserId := currentUser.ID
 
 	time := time.Now()
-	path := fmt.Sprintf("images/%d-%d-%d-%d-%d-%s", UserId, time.Year(), time.Month(), time.Day(), rand.Int(), file.Filename)
+	path := fmt.Sprintf("images/%d-%d-%d-%d-%d-%s", UserId, time.Year(), time.Month(), time.Day(), rand.Intn(10000), file.Filename)
 	fmt.Println(path)
 
 	err = c.SaveUploadedFile(file, path)
