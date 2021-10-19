df0617c7cec2395e2e282d28fc5f36f03964af12
faisal-t <faizalmahromi35@gmail.com>
2021-10-20 00:31:44 +0700

    add campaign handler to main.go

--- handler/campaign.go
+++ handler/campaign.go
@@ -1,1 +1,33 @@
 package handler
+
+import (
+	"bwa-golang/campaign"
+	"bwa-golang/helper"
+	"net/http"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+)
+
+type campaignHandler struct {
+	service campaign.Service
+}
+
+func NewCampaignHandler(service campaign.Service) *campaignHandler {
+	return &campaignHandler{service}
+}
+
+//api/v1/campaigns
+func (h *campaignHandler) GetCampaigns(c *gin.Context) {
+	userID, _ := strconv.Atoi(c.Query("user_id"))
+
+	campaigns, err := h.service.GetCampaigns(userID)
+	if err != nil {
+		response := helper.ApiResponse("Failed To Load Campaigns", http.StatusOK, "error", nil)
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	response := helper.ApiResponse("List Of Campaign", http.StatusOK, "error", campaigns)
+	c.JSON(http.StatusOK, response)
+}
