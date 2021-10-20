7b582194489b08176acd8b0de11db92475fc263d
faisal-t <faizalmahromi35@gmail.com>
2021-10-20 23:06:25 +0700

    add slug

--- main.go
+++ main.go
@@ -38,6 +38,7 @@ func main() {
 	fmt.Println(len(campaigns))
 
 	router := gin.Default()
+	router.Static("/images", "./images")
 	api := router.Group("/api/v1")
 
 	api.POST("/users", userHandler.RegisterUser)
