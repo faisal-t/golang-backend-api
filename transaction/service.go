c1049561c9929b3be666cd5972ef8361d41daeb1
faisal-t <faizalmahromi35@gmail.com>
2021-11-01 16:32:35 +0700

    transaction endpoint

--- transaction/service.go
+++ transaction/service.go
@@ -1,1 +1,13 @@
 package transaction
+
+type service struct {
+	repository Repository
+}
+
+type Service interface {
+	GetTransactionByCampaignID(campaignID int) ([]Transaction, error)
+}
+
+func NewService(repository Repository) *service {
+	return &service{repository}
+}
